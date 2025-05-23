8dceaf243bc14c0a72fac1050e8f7a1e8b8c8c8e
Nathan Novais Borges <nathan-borges2011@live.com>
2025-05-23 01:14:46 -0300

    Create file to deploy backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,17 @@
+import os
 from app import create_app
 from flask_cors import CORS
 
 app = create_app()
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    env = os.environ.get('ENV', 'local')  # default é local
+
+    if env == 'local':
+        # Rodando localmente
+        print("Running in LOCAL environment")
+        app.run(host='0.0.0.0', port=port, debug=True)  # debug ativo localmente
+    else:
+        # Rodando no GCP (produção)
+        print("Running in PRODUCTION environment")
+        app.run(host='0.0.0.0', port=port)
---
+++ backend/deploy.ps1
@@ -0,0 +1,28 @@
+# Set variables
+$REGION = "us-central1"
+$PROJECT_ID = (gcloud config get-value project)
+$IMAGE_NAME = "flask-app-fiap-3"
+$REPO_NAME = "flask-repo"
+
+Write-Host "Selected project: $PROJECT_ID"
+Write-Host "Region: $REGION"
+Write-Host "Image name: $IMAGE_NAME"
+
+# Create image repository (ignore error if it already exists)
+Write-Host "Creating repository (if needed)..."
+gcloud artifacts repositories create $REPO_NAME `
+  --repository-format=docker `
+  --location=$REGION `
+  2>$null
+
+# Build and push Docker image
+Write-Host "Building the image and pushing to Artifact Registry..."
+gcloud builds submit --tag "$REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME" .
+
+# Deploy to Cloud Run
+Write-Host "Deploying to Cloud Run..."
+gcloud run deploy flask-service `
+  --image "$REGION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME" `
+  --platform managed `
+  --region $REGION `
+  --allow-unauthenticated
